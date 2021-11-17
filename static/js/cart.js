6042b8d85c52dbae214c446e5d9f48b9e42b10e2
Siren <siren@kernal.eu>
2021-11-17 20:54:23 +0200

    Fix the bug resulting in incorrect XMR price value in js

--- static/js/cart.js
+++ static/js/cart.js
@@ -14,18 +14,18 @@ else {
 	
 	cart.forEach((element, index) => {
 		if (items.has(element.id)) {
-			items.set(element.id, [element.title, element.price, items.get(element.id)[3] + 1, index])
+			items.set(element.id, [element.title, element.price, items.get(element.id)[2] + 1, index])
 		} else {
 			items.set(element.id, [element.title, element.price, 1, index])
 		}
-		total += element.price
+		total += element.atomic
 	})
 	
 	var i
 	items.forEach((value, key, map) => {
 		i++
 		console.log(value, key)
-		h = '<div id="item"><div id="title">' + value[3] + ' x '+ value[0]
+		h = '<div id="item"><div id="title">' + value[2] + ' x '+ value[0]
 			+ ' ' + value[1] + ' XMR</div><div class="remove" id="' 
 			+ i + '">x</div></div>'
 		list.innerHTML += h
@@ -34,6 +34,6 @@ else {
 	f = `<input type="text" id="cart" name="cart" value='` + c.replace(/'/g, "&#39;") + `' hidden><br>`
 	form.innerHTML += f
 
-	h = '<h3>Total: ' + total + ' XMR</h3>'
+	h = '<h3>Total: ' + parseFloat(total) / 1e12 + ' XMR</h3>'
 	list.innerHTML += h
 }
--- static/js/product.js
+++ static/js/product.js
@@ -3,7 +3,8 @@ document.getElementById("button").addEventListener("click", function(){
     var cart = local ? JSON.parse(local) : [];
     cart.push({id: parseInt(document.getElementById("id").getAttribute("value")),
         title: document.getElementById("title").getAttribute("value"),
-        price: parseFloat(document.getElementById("price").getAttribute("value"))
+        price: parseFloat(document.getElementById("price").getAttribute("value")),
+	atomic: parseInt(document.getElementById("atomic").getAttribute("value"), 10)
     });
     localStorage.setItem("cart", JSON.stringify(cart));
 });
