e458d54d85947f09ca0d754fbb0e044bbc73a024
Siren <siren@kernal.eu>
2021-11-22 23:46:59 +0200

    Rewrite viewing, adding to cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,39 +1,75 @@
 var c = localStorage.getItem('cart')
 var form = document.getElementById('form')
 var list = document.getElementById('listing')
+var total = document.getElementById('total')
 var cart
-const items = new Map()
+var sum
+
+function registerEvents() {
+	[...document.querySelectorAll('.remove')].forEach(element => {
+		element.addEventListener('click', deleteItem)
+	})
+}
+
+function updateItem(element) {
+	var listing = document.getElementById("item-" + element.id)
+	var quantity = parseInt(element.getAttribute('quantity'), 10) - 1
+	if (quantity == 0) {
+		listing.remove()
+		return
+	}
+
+	var title = element.getAttribute('title')
+	var price = element.getAttribute('price')
+	var atomic = element.getAttribute('atomic')
+
+	listing.innerHTML = '<div class="title">' 
+		+ quantity + ' x ' + title + ' ' + price
+		+ ' XMR</div><div class="remove" id="'
+		+ element.id + '" title="' + title 
+		+ '" quantity="' + quantity 
+		+ '" price="' + price 
+		+ '" atomic="' + atomic
+		+ '">x</div>'
+	
+	registerEvents()
+}
+
+function updateTotal(val) {
+	total.innerHTML = 'Total: ' + parseFloat(val) / 1e12 + ' XMR'
+}
+
+function deleteItem() {
+	sum -= parseInt(this.getAttribute('atomic'), 10)
+	updateTotal(sum)
+	updateItem(this)
+}
 
 if (c == null) {
 	form.style.display = "none"
 	list.innerHTML += "<p>Empty cart, can't find anything you like?</p>"
-}
-else {
+} else {
 	cart = JSON.parse(c)
-	var total = 0
+	sum = 0
 	
 	cart.forEach((element, index) => {
-		if (items.has(element.id)) {
-			items.set(element.id, [element.title, element.price, items.get(element.id)[2] + 1, index])
-		} else {
-			items.set(element.id, [element.title, element.price, 1, index])
-		}
-		total += element.atomic
-	})
-	
-	var i
-	items.forEach((value, key, map) => {
-		i++
-		console.log(value, key)
-		h = '<div id="item"><div id="title">' + value[2] + ' x '+ value[0]
-			+ ' ' + value[1] + ' XMR</div><div class="remove" id="' 
-			+ i + '">x</div></div>'
+		h = '<div class="cartitem" id="item-' + element.id 
+			+ '"><div class="title">' + element.quantity 
+			+ ' x ' + element.title + ' ' + element.price
+			+ ' XMR</div><div class="remove" id="'
+			+ element.id + '" title="' + element.title
+			+ '" quantity="' + element.quantity
+			+ '" price="' + element.price
+			+ '" atomic="' + element.atomic + '">x</div></div>'
+
 		list.innerHTML += h
+		sum += element.atomic * element.quantity
 	})
 
+	registerEvents()
+	
 	f = `<input type="text" id="cart" name="cart" value='` + c.replace(/'/g, "&#39;") + `' hidden><br>`
 	form.innerHTML += f
 
-	h = '<h3>Total: ' + parseFloat(total) / 1e12 + ' XMR</h3>'
-	list.innerHTML += h
+	updateTotal(sum)
 }
--- static/js/product.js
+++ static/js/product.js
@@ -1,10 +1,33 @@
-document.getElementById("button").addEventListener("click", function(){
-    var local = localStorage.getItem("cart");
-    var cart = local ? JSON.parse(local) : [];
-    cart.push({id: parseInt(document.getElementById("id").getAttribute("value")),
-        title: document.getElementById("title").getAttribute("value"),
-        price: parseFloat(document.getElementById("price").getAttribute("value")),
-	atomic: parseInt(document.getElementById("atomic").getAttribute("value"), 10)
-    });
-    localStorage.setItem("cart", JSON.stringify(cart));
-});
+document.getElementById("button").addEventListener("click", function(){	
+	var id = parseInt(document.getElementById("id").getAttribute("value"))
+        var title = document.getElementById("title").getAttribute("value")
+        var price = parseFloat(document.getElementById("price").getAttribute("value"))
+	var atomic = parseInt(document.getElementById("atomic").getAttribute("value"), 10)
+
+	var local = localStorage.getItem("cart");
+	var cart = []
+	var found = false
+
+	if (local == null) {
+		cart.push({id: id, title: title, price: price,
+			atomic: atomic, quantity: 1})
+    		localStorage.setItem("cart", JSON.stringify(cart));
+		return
+	}
+
+	cart = JSON.parse(local)
+
+	cart.forEach((element, index) => {
+		if (element.id == id) {
+                	element.quantity++
+			found = true
+		}
+	})
+
+	if (found == false) {
+		cart.push({id: id, title: title, price: price,
+			atomic: atomic, quantity: 1})
+	}
+
+	localStorage.setItem("cart", JSON.stringify(cart));
+})
